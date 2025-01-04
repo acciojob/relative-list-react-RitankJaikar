745a0b13e8f5883a945a411f9791553dc65513d6
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-04 16:00:45 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const App = () => {
+function App() {
+  const relatives = [
+    { id: 1, name: "Amit" },
+    { id: 2, name: "Suman" },
+    { id: 3, name: "Rahul" },
+    { id: 4, name: "Priya" },
+  ];
+
   return (
-    <div id="main">
-               {/* Do not remove the main div */}
+    <div>
+      <h1>Relatives to Visit this Diwali</h1>
+      <ol key="relativeList">
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative.name}</li>
+        ))}
+      </ol>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
